28253fcacd33547196a7e6d9b4718e9606c1a9f8
Janpria Pal <65071921+JP01234-Alpha@users.noreply.github.com>
2024-03-25 21:57:00 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./inde.html",
+    "./src/**/*.{js,ts,jsx,tsx}"
+  ],
+  darkMode: "class",
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
